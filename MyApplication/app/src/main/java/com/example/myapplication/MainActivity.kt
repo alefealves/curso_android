4d928ee14b09f3ec8729fb29eb485d4327b2278b
Alefe Alves <alefealves@hotmail.com>
2019-10-10 10:13:24 -0400

    primeiro commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        login.setOnClickListener {
+
+            Log.d(MainActivity::class.java.simpleName, user_name.text.toString())
+            Log.d(MainActivity::class.java.simpleName, user_pass.text.toString())
+
+        }
+    }
+}
